75b60904b306ad00d8c6e98c733cdc05bec16546
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2025-05-29 10:40:36 +0530

    [kuber] feat . student attendance problem

---
+++ student_attendance.py
@@ -0,0 +1,21 @@
+day1 = ["Ankit", "Abhi", "Candy"]
+day2= ["Abhi", "Ankit"]
+day3= ["Candy", "Ankit", "Abhi"]
+
+
+
+days = [day1,day2,day3]
+
+
+attendance = {}
+
+
+
+for day in days:
+    for student in day:
+        if student in attendance:
+            attendance[student] += 1
+        else:
+            attendance[student] = 1
+
+print(f"Total Attendance:{attendance}")
